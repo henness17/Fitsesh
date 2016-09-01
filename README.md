d57ba2341f85e15d8e04431bd4b01f9eb1a9f3e3
Henness <Ryan.Henness@Fiserv.com>
2016-09-01 11:36:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Fitsesh README
---
+++ index.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var app = express();
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
+
+passport.use(new Strategy({
+    clientID: '1032451400184652',
+    clientSecret: '6640678d88db3c3b0d7815f22c4896f2',
+    callbackURL: 'https://fitsesh.herokuapp.com/login/facebook/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+  }));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes.
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+app.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/login/facebook/return', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    req.user.username = "Henny";
+    req.user.exp = 550;
+    req.user.level = Math.round(req.user.exp / 100);
+    req.user.points = 4905;
+    if(req.user.level > 5){
+      req.user.badges = [];
+      req.user.badges.push("Not Such A Noob - You surpassed level 5.");
+    }
+    if(req.user.username == "Henny"){
+      req.user.badges.push("You Are Henny - Your username is Henny.");
+    }
+    res.render('profile', { user: req.user });
+  });
+
+app.get('/buildings',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    req.user.buildings = ["5000 N Willamette Blvd, Portland, OR 97203", "Yale House", "McDonalds Around The Corner"];
+    res.render('buildings', { user: req.user });
+  });
+
+app.get('/building',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    req.user.building = "Shiley Hall";
+    req.user.users = 12;
+    req.user.currentHealth = req.user.users * 100;
+    req.user.maxHealth = req.user.users * 100;
+    req.user.topUsers = ["DenyElectricity", "Henny", "OldBob"];
+    res.render('building', { user: req.user });
+  });
+
+app.get('/leaderboard',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    req.user.topUsers = ["RJ3000", "DenyElectricity", "Tyrone", "Henny", "AntiLight", "OldBob", "xSamuel", "JakeTheSnake", "ShileyWarrior", "JunkCaptain"];
+    res.render('leaderboard', { user: req.user });
+  });
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
