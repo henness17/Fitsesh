47151e00d1a61ef32d1c1f61245d3c0c21a19080
Ryan Henness <ryanhenness@gmail.com>
2016-09-03 19:50:30 -0700

    lol

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get('/',
 	pg.connect(connect, function(err, client) {
 	  if (err) throw err;
 	  console.log('Connected to postgres! Getting schemas...');
-    var getUserById = client.query('SELECT * FROM users WHERE username = "ryan"');
+    var getUserById = client.query('SELECT * FROM users WHERE username="ryan"');
     if(getUserById.length == null){
       client.query('INSERT INTO users(username) VALUES("InputUser")', function(err, result){
         if(err){
